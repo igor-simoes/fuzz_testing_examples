c86be91cd880c4618a93b2f1b3a13dfcaa97a502
Marcelo d'Amorim <damorim.marcelo@gmail.com>
2018-05-25 17:48:00 -0300

    polishing example

--- booklibrary2/book.cc
+++ booklibrary2/book.cc
@@ -1,3 +1,8 @@
+#include <list>
+#include <vector>
+#include <string.h>
+#include <iostream>
+
 #include "book.h"
 
 Author::Author(string const& _name) : name(_name) { }
@@ -8,7 +13,17 @@ Library::Library(Book* _books) : books(_books) { }
 
 Book* Library::findTitles(string const& name, Book* books, int size)
 {
-    
-
-    return 0;
+    std::list<Book> authorBooks = {};
+    for (int i = 0; i < size; i++)
+    {
+        if (strcmp(name.c_str(), books[i].author.name.c_str()) == 0)
+            authorBooks.push_back(books[i]);
+    }
+    if (authorBooks.size() == 0)
+        return NULL;
+    if (authorBooks.size() == 2)
+        fprintf(stderr, "this is an artificial bug!");
+        abort();        
+    std::vector<Book> v{ std::begin(authorBooks), std::end(authorBooks) };
+    return &v.at(0);
 }
--- booklibrary2/booklibrary_target.cc
+++ booklibrary2/booklibrary_target.cc
@@ -10,32 +10,28 @@ template<size_t SIZE, class T> inline size_t array_size(T (&arr)[SIZE]) {
 }
 
 extern "C" int LLVMFuzzerTestOneInput(const u_int8_t* Data, size_t size) {
-    if (size < 7) return 0;
+    if (size < 22) return 0;
 
     // reading data
-    char author1[4];
-    author1[0] = (char) Data[0];
-    author1[1] = (char) Data[1];
-    author1[2] = (char) Data[2];
-    author1[3] = '\0';
+    string author1 = {(char) Data[0], (char) Data[1], (char) Data[2], (char) Data[3], (char) Data[4]};
+    string booktitle1 = {(char) Data[5], (char) Data[6], (char) Data[7]};
+    string booktitle2 = {(char) Data[8], (char) Data[9], (char) Data[10]};
 
-    char booktitle1[4];
-    booktitle1[0] = (char) Data[3];
-    booktitle1[1] = (char) Data[4];
-    booktitle1[2] = (char) Data[5];
-    booktitle1[3] = '\0';
-
-    char booktitle2[4];    
-    booktitle2[0] = (char) Data[6];
-    booktitle2[1] = (char) Data[7];
-    booktitle2[2] = (char) Data[8];
-    booktitle2[3] = '\0';
+    string author2 = {(char) Data[11], (char) Data[12], (char) Data[13], (char) Data[14], (char) Data[15]};
+    string booktitle3 = {(char) Data[16], (char) Data[17], (char) Data[18]};
+    string booktitle4 = {(char) Data[19], (char) Data[20], (char) Data[21]};
     
     // building objects
-    Author a(author1);
-    Book b1(booktitle1, a);
-    Book b2(booktitle2, a);    
-    Book books[] = {b1};
+    Author a1(author1);
+    Book b1(booktitle1, a1);
+    Book b2(booktitle2, a1);    
+
+    Author a2(author2);
+    Book b3(booktitle3, a2);
+    Book b4(booktitle4, a2);    
+
+    Book books[] = {b1, b2, b3, b4};
+    
     Library lib(books);
 
     // calling function
